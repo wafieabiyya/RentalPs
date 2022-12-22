bd4e3fec2a21c1b995ca82e66d788dfc423f4989
faqihfaizal <mochamad.faqih@mhs.itenas.ac.id>
2022-12-22 11:41:32 +0700

    add feature customer

--- src/main/java/com/mycompany/projectuas/serviceimpl/CustomerServiceImpl.java
+++ src/main/java/com/mycompany/projectuas/serviceimpl/CustomerServiceImpl.java
@@ -4,10 +4,139 @@
  */
 package com.mycompany.projectuas.serviceimpl;
 
+import com.mycompany.projectuas.pojo.Customer;
+import com.mycompany.projectuas.service.CustomerService;
+import com.mycompany.projectuas.utilities.ConnectionManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author lenovo
  */
-public class CustomerServiceImpl {
+public class CustomerServiceImpl implements CustomerService{
+    private ConnectionManager conMan;
+    private Connection conn;
+    Statement stmt;
+    ResultSet rs;
     
+    @Override
+    public List<Customer> findAll() {
+        List<Customer> listCustomer = new ArrayList<>();
+        String query = "SELECT *FROM customer";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        try {
+         stmt = conn.createStatement();
+         rs = stmt.executeQuery(query);
+         while (rs.next()){
+             Customer customer = new Customer();
+             customer.setId(rs.getInt("ID_customer"));
+             customer.setNamaCustomer(rs.getString("nama_customer"));
+             
+             listCustomer.add(customer);
+         }
+         conMan.dc();
+        } 
+        catch (SQLException e) {
+            Logger.getLogger(ConnectionManager.class.getName()).
+                    log(Level.SEVERE, null,e);
+        }
+        
+      return listCustomer;
+    }
+    
+    @Override
+    public Integer create(Customer object) {
+        int result = 0;
+        String query = "INSERT INTO customer (ID_customer, nama_customer, alamat_customer) "
+                + "VALUES"
+                + "("+object.getId()
+                +",'"+object.getNamaCustomer()
+                +",'"+object.getNamaCustomer()+"')";
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate(query);
+            conMan.dc();
+        } 
+        catch (SQLException e) {
+            Logger.getLogger(CustomerService.class.getName()).
+                    log(Level.SEVERE, null, e);
+        }
+        return result;
+    }
+
+    @Override
+    public Integer update(Customer object) {
+    int result = 0;
+        
+        String query = "UPDATE customer Set nama_customer ="
+                + "'"+object.getNamaCustomer()
+                +"', alamat_customer='"+object.getAlamat()+"'"
+                + "WHERE ID_customer = "+object.getId()+"";
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            stmt = conn.createStatement();
+           result = stmt.executeUpdate(query);
+            conMan.dc();
+        } catch (SQLException e) {
+            Logger.getLogger(CustomerServiceImpl.class.getName()).
+                    log(Level.SEVERE, null,e);
+        }
+        return result;
+    }
+
+    @Override
+    public Customer findById(int id) {        
+    Customer customer = null;
+        String query = "SELECT * FROM customer WHERE ID_customer = " +id+"";
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(query);
+            while (rs.next()){
+                
+                customer = new Customer();
+                customer.setId(rs.getInt("ID_customer"));
+                customer.setNamaCustomer(rs.getString("nama_customer"));
+                customer.setAlamat(rs.getString("alamat_customer"));
+            }
+            conMan.dc();
+        } catch (SQLException e) {
+            Logger.getLogger(CustomerService.class.getName()).
+                    log(Level.SEVERE, null, e);
+        }
+        return customer;
+    }
+
+    @Override
+    public Integer delete(int id) {
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    int result = 0;
+        String query = "DELETE customer WHERE ID_customer = "+id+"";
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        try {
+            stmt = conn.createStatement();
+            result = stmt.executeUpdate(query);
+            conMan.dc();
+        } catch (SQLException e) {
+            Logger.getLogger(CustomerServiceImpl.class.getName()).
+                    log(Level.SEVERE, null, e);
+        }
+        return result;
+    }
+
 }
